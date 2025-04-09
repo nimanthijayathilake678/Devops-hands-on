508c9f296b380a87d72880fe838c1c5d1555926d
nimanthijayathilake678 <jayathilake.nimanthi99@gmail.com>
2025-04-09 15:43:33 +0530

    Updated for testing

--- app/app.js
+++ app/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello, DevOps!'));
+app.get('/', (req, res) => res.send('Hello, DevOps! test '));
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
